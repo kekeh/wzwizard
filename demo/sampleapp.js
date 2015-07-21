bbb74447b98b7cee6aca0caaea966d565fe18936
kekeh <kea@live.fi>
2015-07-21 17:48:10 +0300

    Demo added.

---
+++ demo/sampleapp.js
@@ -0,0 +1,93 @@
+/**
+ * @ngdoc object
+ * @name sampleapp
+ * @description Module which uses the vsdatatable component.
+ */
+var sampleapp = angular.module('sampleapp', ['wzwizard']);
+
+/**
+ * @ngdoc object
+ * @name samplectrl
+ * @description samplectrl is controller of sampleapp.
+ */
+sampleapp.controller('samplectrl', function ($scope) {
+
+    $scope.values = {
+        firstname: '',
+        lastname: '',
+        gender: '',
+        streetaddress: '',
+        zip: '',
+        city: '',
+        email: '',
+        phone: ''
+    };
+
+
+    function onIsPageDataValidFn(pageIdx) {
+        console.log('onIsPageDataValidFn(): ', pageIdx);
+        if (pageIdx === 0) {
+            if ($scope.values.firstname === '') {
+                return {result: false, message: 'First name: value required!'};
+            }
+            else if ($scope.values.lastname === '') {
+                return {result: false, message: 'Last name: value required!'};
+            }
+            else if ($scope.values.gender === '') {
+                return {result: false, message: 'Gender: value required!'};
+            }
+            else {
+                return {result: true};
+            }
+        }
+        else if (pageIdx === 1) {
+            if ($scope.values.streetaddress === '') {
+                return {result: false, message: 'Street address: value required!'};
+            }
+            else if ($scope.values.zip === '') {
+                return {result: false, message: 'Zip: value required!'};
+            }
+            else if ($scope.values.city === '') {
+                return {result: false, message: 'City: value required!'};
+            }
+            else {
+                return {result: true};
+            }
+        }
+        else if (pageIdx === 2) {
+            if ($scope.values.email === '') {
+                return {result: false, message: 'Email: value required!'};
+            }
+            else if ($scope.values.phone === '') {
+                return {result: false, message: 'Phone: value required!'};
+            }
+            else {
+                return {result: true};
+            }
+        }
+    }
+
+    function onPageChangedFn(oldPageIdx, newPageIdx) {
+        console.log('oldPageIdx: ', oldPageIdx, ' - newPageIdx: ', newPageIdx);
+    }
+
+    function onAcceptBtnFn() {
+        console.log('onAcceptBtnFn() called!');
+        return {result: true, message: 'Thanks, well done.'};
+    }
+
+    // Configuration of the wzwizard
+    $scope.opt = {
+        backBtnText: 'Back',
+        nextBtnText: 'Next',
+        acceptBtn: {
+            showAcceptBtn: true,
+            acceptBtnText: 'Save',
+            acceptBtnCb: onAcceptBtnFn
+        },
+        showPageNumber: true,
+        isPageDataValidCb: onIsPageDataValidFn,
+        pageChangedCb: onPageChangedFn
+    };
+
+});
---
+++ demo/wzwizard-0.0.3.js
@@ -0,0 +1,169 @@
+/* 
+ *  Name: wzwizard
+ *  Description: Wizard - AngularJS reusable UI component
+ *  Version: 0.0.3
+ *  Author: kekeh
+ *  Homepage: http://kekeh.github.io/wzwizard
+ *  License: MIT
+ *  Date: 2015-07-21
+ */
+angular.module('template-wzwizard-0.0.3.html', ['templates/wzpage.html', 'templates/wzwizard.html']);
+
+angular.module("templates/wzpage.html", []).run(["$templateCache", function($templateCache) {
+    $templateCache.put("templates/wzpage.html",
+        "<div ng-show=\"visible\" ng-transclude></div>\n" +
+        "");
+}]);
+
+angular.module("templates/wzwizard.html", []).run(["$templateCache", function($templateCache) {
+    $templateCache.put("templates/wzwizard.html",
+        "<div class=\"wzwizard\">\n" +
+        "    <div class=\"wzpages\">\n" +
+        "        <div class=\"wzpage\" ng-repeat=\"p in wzpages\" ng-style=\"{'width': 100/wzpages.length + '%'}\" ng-class=\"{wzactive:p.visible}\">\n" +
+        "            <span class=\"wztitletext\" ng-if=\"opt.showPageNumber\" style=\"margin-right:4px\">\n" +
+        "                {{$index+1}}{{config.PAGE_NUMBER_SEPARATOR}}\n" +
+        "            </span>\n" +
+        "            <span class=\"wztitletext\">\n" +
+        "                {{p.title}}\n" +
+        "            </span>\n" +
+        "        </div>\n" +
+        "    </div>\n" +
+        "\n" +
+        "    <div class=\"wzcontent\" ng-transclude></div>\n" +
+        "    <div ng-class=\"{'wzok':response.result,'wzerror':!response.result}\" ng-if=\"response.message!==undefined && response.message!==''\">\n" +
+        "        {{response.message}}\n" +
+        "        <span class=\"icon icon-cross\" ng-click=\"response.message=''\"></span>\n" +
+        "    </div>\n" +
+        "    \n" +
+
+        "\n" +
+        "    <div class=\"wzfooter\">\n" +
+        "        <button class=\"wzfooterbtn\" ng-click=\"backBtnClicked()\" ng-if=\"visiblePageIdx>0\">\n" +
+        "            {{opt.backBtnText}}\n" +
+        "        </button>\n" +
+        "        <button class=\"wzfooterbtn\" style=\"margin-left:4px\" ng-click=\"nextBtnClicked()\" ng-if=\"visiblePageIdx<wzpages.length-1\">\n" +
+        "            {{opt.nextBtnText}}\n" +
+        "        </button>\n" +
+        "        <button class=\"wzfooterbtn\" style=\"margin-left:4px\" ng-click=\"acceptBtnClicked()\" ng-if=\"opt.acceptBtn.showAcceptBtn&&visiblePageIdx===wzpages.length-1\">\n" +
+        "            {{opt.acceptBtn.acceptBtnText}}\n" +
+        "        </button>\n" +
+        "    </div>\n" +
+        "</div>\n" +
+        "");
+}]);
+
+/**
+ * @ngdoc object
+ * @name wzwizard
+ * @description wzwizard is module of wzwizard.
+ */
+angular.module('wzwizard', ["template-wzwizard-0.0.3.html"])
+
+/**
+ * @ngdoc object
+ * @name wzwizardConfig
+ * @description wzwizardConfig constants of the date picker.
+ */
+    .constant('wzwizardConfig', {
+        PAGE_NUMBER_SEPARATOR: '.'
+    })
+
+/**
+ * @ngdoc object
+ * @name wzwizard
+ * @description wzwizard is main directive of the component and it implements the wizard.
+ */
+    .directive('wzwizard', function () {
+        return {
+            restrict: 'EA',
+            templateUrl: 'templates/wzwizard.html',
+            transclude: true,
+            scope: {
+                opt: '=options'
+            },
+            controller: ['$scope', 'wzwizardConfig', function ($scope, wzwizardConfig) {
+                $scope.config = wzwizardConfig;
+                $scope.wzpages = [];
+                $scope.visiblePageIdx = 0;
+                $scope.response = {result: true, message: ''};
+
+                // Add pages to the array
+                this.addPage = function (page) {
+                    page.visible = $scope.wzpages.length === 0 ? true : false;
+                    $scope.wzpages.push(page);
+                };
+            }],
+            link: function (scope, element, attrs) {
+                scope.backBtnClicked = function () {
+                    resetError();
+                    back();
+                    pageChanged(scope.visiblePageIdx + 1, scope.visiblePageIdx);
+                };
+
+                scope.nextBtnClicked = function () {
+                    scope.response = isPageDataValid();
+                    if (scope.response.result) {
+                        next();
+                        pageChanged(scope.visiblePageIdx - 1, scope.visiblePageIdx);
+                    }
+                };
+
+                scope.acceptBtnClicked = function () {
+                    if (scope.opt.acceptBtn.acceptBtnCb) {
+                        var resp = scope.opt.acceptBtn.acceptBtnCb();
+                        if (angular.isObject(resp)) {
+                            scope.response = resp;
+                        }
+                    }
+                };
+
+                function back() {
+                    scope.wzpages[scope.visiblePageIdx--].visible = false;
+                    scope.wzpages[scope.visiblePageIdx].visible = true;
+                }
+
+                function next() {
+                    scope.wzpages[scope.visiblePageIdx++].visible = false;
+                    scope.wzpages[scope.visiblePageIdx].visible = true;
+                }
+
+                function isPageDataValid() {
+                    if (scope.opt.isPageDataValidCb) {
+                        return scope.opt.isPageDataValidCb(scope.visiblePageIdx);
+                    }
+                    return true;
+                }
+
+                function pageChanged(oldPageIdx, newPageIdx) {
+                    if (scope.opt.pageChangedCb) {
+                        scope.opt.pageChangedCb(oldPageIdx, newPageIdx);
+                    }
+                }
+
+                function resetError() {
+                    scope.response = {valid: true, message: ''};
+                }
+            }
+        };
+    })
+/**
+ * @ngdoc object
+ * @name wzpage
+ * @description wzpage directive implements one page of wizard.
+ */
+    .directive('wzpage', function () {
+        return {
+            require: '^wzwizard',
+            restrict: 'E',
+            transclude: true,
+            scope: {},
+            link: function (scope, element, attrs, ctrl) {
+                scope.title = attrs.title !== undefined ? attrs.title : '';
+                ctrl.addPage(scope);
+            },
+            templateUrl: 'templates/wzpage.html'
+        };
+    });
+
+
+
